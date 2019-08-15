ac0e5cb89c16031ef84b715da059eb0388e1924d
broliang <18706703437@163.com>
2019-08-15 20:58:23 +0800

    Rewrite 01_which_shell_am_I_runing_when_login using Expect

--- 01_basic_unix_environment/01_introduction/01_which_shell_am_I_runing_when_login.sh
+++
@@ -1,9 +0,0 @@
-login_name=$1
-
-cmd="grep $login_name /etc/passwd"
-
-echo $($cmd)
-
-# You should get back the contents of your entry in the system password file. For example:
-# shelleyp:*:1006:1006:Shelley Powers:/usr/home/shelleyp:/usr/local/bin/bash
-# The fields are separated by colons, and the default shell is usually specified in the last field.
