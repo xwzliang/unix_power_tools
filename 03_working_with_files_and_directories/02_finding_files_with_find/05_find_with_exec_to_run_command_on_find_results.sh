ffaa819f055b1348b2aec0629aeed4ddfe5f7b69
broliang <18706703437@163.com>
2019-09-05 20:37:17 +0800

    New 05_find_with_exec_to_run_command_on_find_results.sh

---
+++ 03_working_with_files_and_directories/02_finding_files_with_find/05_find_with_exec_to_run_command_on_find_results.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env  bash
+
+# -exec command must end with ;, and we need backslash or quotes to avoid shell using it.
+# {} refers to the results find returns
+find . -exec echo {} \;
+find . -exec echo {} ';'
+
+# -exec can also run find itself
+# If you wanted to list every symbolic link in every directory owned by a group staff under the current directory, you could execute:
+find `pwd` -type d -group staff -exec find {} -type l -print \;
+
+# To search for all files with group-write permission under the current directory and to remove the permission, you can use:
+find . -perm -20 -exec chmod g-w {} \;
+find . -perm -20 -print | xargs chmod g-w
+# The difference between -exec and xargs is subtle. The first one will execute the program once per file, while xargs can handle several files with each process. However, xargs may have problems with filenames that contain embedded spaces. (Versions of xargs that support the -0 option can avoid this problem; they expect NUL characters as delimiters instead of spaces, and find 's -print0 option generates output that way.)
+
+# Occasionally, people create a strange file that they can't delete. This could be caused by accidentally creating a file with a space or some control character in the name. find and -exec can delete this file, while xargs could not. In this case, use ls -il to list the files and i-numbers, and use the -inum operator with -exec to delete the file:
+find . -inum 31246 -exec rm {} ';'
+# If you wish, you can use -ok , which does the same as -exec, except the program asks you to confirm the action first before executing the command.
+
+
+# Remember that -exec doesn't necessarily evaluate to "true"; it only evaluates to true if the command it executes returns a zero exit status. You can use this to construct custom find tests. Assume that you want to list files that are "beautiful." You have written a program called beauty that returns zero if a file is beautiful and nonzero otherwise.
+find . -exec beauty {} \;
+
+
+# Use find 's -exec for large recursive greps. Let's say I want to search through a large directory with lots of subdirectories to find all of the .cc files that call the method GetRaw( ):
+find . -name \*.cc -exec grep -n "GetRaw(" {} \;
+# it shows me each line that matched my grep along with its line number, followed by the name of the file where those lines were found.
+# Most versions of grep can search recursively (using -R), but they search all files; you need find to grep through only certain files in a large directory tree.
+
